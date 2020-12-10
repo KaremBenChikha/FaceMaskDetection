452b324d1ea7758c79c12bf67236c55878a34ef0
karem <karem.benchikha@gmail.com>
2020-12-10 14:59:51 +0100

    just another useless commit

--- DetecMask.py
+++ DetecMask.py
@@ -1,12 +1,8 @@
 # import the necessary packages
-import tensorflow.keras.applications.mobilenet_v2
-import tensorflow.keras.preprocessing.mobilenet_v2
-import tensor.keras.models
-import imutils.video
-#from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-#from tensorflow.keras.preprocessing.image import img_to_array
-#from tensorflow.keras.models import load_model
-#from imutils.video import VideoStream
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
 import numpy as np
 import imutils
 import time
